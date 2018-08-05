f279cf6f220cb0308aeff7df0d77983e8446a501
Donald J. Trump <Ilemy939@gmail.com>
2018-08-05 08:21:00 -0400

    Update Main.php

--- src/Wolf/ShopUI/Main.php
+++ src/Wolf/ShopUI/Main.php
@@ -7,4 +7,8 @@ use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+//*Must Have FormAPI to Work*//
 use jojoe77777\FormAPI;
+
+class Main extends PluginBase implements Listener{
