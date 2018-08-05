8a9a18d6135f97cce161408c568ceb4d208f9919
Wolfie <Wolfkid200444@gmail.com>
2018-08-05 12:28:47 -0400

    Add files via upload

--- src/Wolf/Main.php
+++ src/Wolf/Main.php
@@ -1,1 +1,27 @@
+<?php 
 
+namespace Wolf;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\{command\ConsoleCommandSender, Server, Player, utils\TextFormat};
+use pocketmine\plugin\PluginBase;
+use jojoe77777\FormAPI;
+
+class Main extends PluginBase implements Listener{
+	
+	public function onEnable(){
+		$this->getServer()->getLogger()->Info("§aUIHelp has been enabled!\n\n\n\n\§l§b§onSubcribe channel Wolfkid!!");
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	
+	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
+		$player = $sender->getPlayer();
+		switch($cmd->getName()){
+			case "helpui":
+			$this->mainForm($player);
+        }
+        return true;
+    }
+	
