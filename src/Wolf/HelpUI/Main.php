1df2d0d62e58cf11b64f9d894f09c8d920ba86ff
Donald J. Trump <Ilemy939@gmail.com>
2018-08-05 09:24:30 -0400

    Add files via upload

--- src/Wolf/HelpUI/Main.php
+++ src/Wolf/HelpUI/Main.php
@@ -1,1 +1,25 @@
+?help
 
+ namespace ShopUI;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+//*Must Have FormAPI to Work*//
+use jojoe77777\FormAPI;
+
+class Main extends PluginBase implements Listener{
+
+   public function onEnable() {
+       $this->getLogger()->Info("HelpUI is ready to Help")
+       $this->getServer()->getPluginManager()->registerEvents($this, $this);
+   }
+  
+   public funtion onDisable() {
+       $this->getLogger()->Info("HelpUI has been Disable WHYY")
+   }
+  
+   public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args)
